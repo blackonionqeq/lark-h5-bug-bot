8cf6e354e37e906ee536b4868d5c1e4ec8fe0a48
blackoniont-t <2129733406@qq.com>
2026-05-13 09:44:19 +0700

    add pm2 config for code dir

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,19 @@
 module.exports = {
   apps: [
     {
+      name: "lark-h5-bug-bot-cloud",
+      cwd: `${__dirname}/code`,
+      script: "pnpm",
+      args: "start",
+      interpreter: "none",
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: "10s",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
       name: "lark-h5-bug-bot-worker",
       cwd: `${__dirname}/worker`,
       script: "pnpm",
